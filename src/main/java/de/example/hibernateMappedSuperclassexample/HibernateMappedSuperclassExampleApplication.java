ba58e10ffc6501565f2d1ad3f6b934d906223736
GieHan <Gie.Hanggoro@Hotmail.com>
2022-07-13 16:29:36 +0200

    Lets start

---
+++ src/main/java/de/example/hibernateMappedSuperclassexample/HibernateMappedSuperclassExampleApplication.java
@@ -0,0 +1,13 @@
+package de.example.hibernateMappedSuperclassexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMappedSuperclassExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateMappedSuperclassExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/example/hibernateMappedSuperclassexample/HibernateMappedSuperclassExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.example.hibernateMappedSuperclassexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMappedSuperclassExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
